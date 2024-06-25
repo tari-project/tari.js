6d3e29deab62896681259026d212c2fe7a2d1ae6
Maciej Kożuszek <mackozuszek@gmail.com>
2024-06-25 14:16:57 +0200

    feat: add request for parent size (#15)

--- src/providers/tari_universe/provider.ts
+++ src/providers/tari_universe/provider.ts
@@ -1,4 +1,3 @@
-import { TariPermissions } from "../wallet_daemon/tari_permissions";
 import {
   SubmitTransactionRequest,
   TransactionResult,
@@ -8,22 +7,32 @@ import {
   TemplateDefinition,
   VaultBalances,
 } from "../types";
-import { ProviderRequest, ProviderMethodNames, ProviderReturnType, ProviderResponse } from "./types";
+import {
+  ProviderRequest,
+  ProviderMethodNames,
+  ProviderReturnType,
+  ProviderResponse,
+  TariUniverseProviderParameters,
+  WindowSize,
+} from "./types";
 import { TariProvider } from "../index";
 import { AccountsGetBalancesResponse } from "@tariproject/wallet_jrpc_client";
-
-export type TariUniverseProviderParameters = {
-  permissions: TariPermissions;
-  optionalPermissions: TariPermissions;
-  name?: string;
-  onConnection?: () => void;
-};
 
 export class TariUniverseProvider implements TariProvider {
   public providerName = "TariUniverse";
   private __id = 0;
 
-  public constructor(public params: TariUniverseProviderParameters) {}
+  public constructor(public params: TariUniverseProviderParameters) {
+    const filterResizeEvent = function (event: MessageEvent) {
+      if (event.data && event.data.type === "resize") {
+        const resizeEvent = new CustomEvent("resize", {
+          detail: { width: event.data.width, height: event.data.height },
+        });
+        window.dispatchEvent(resizeEvent);
+      }
+    };
+    window.addEventListener("message", (event) => filterResizeEvent(event), false);
+  }
 
   private async sendRequest<MethodName extends ProviderMethodNames>(
     req: Omit<ProviderRequest<MethodName>, "id">,
@@ -31,14 +40,14 @@ export class TariUniverseProvider implements TariProvider {
     const id = ++this.__id;
     return new Promise<ProviderReturnType<MethodName>>(function (resolve, _reject) {
       const event_ref = function (resp: MessageEvent<ProviderResponse<MethodName>>) {
-        if (resp && resp.data && resp.data.id && resp.data.id == id) {
+        if (resp && resp.data && resp.data.id && resp.data.id == id && resp.data.type === "provider-call") {
           window.removeEventListener("message", event_ref);
           resolve(resp.data.result);
         }
       };
       window.addEventListener("message", event_ref, false);
 
-      window.parent.postMessage({ ...req, id }, "*");
+      window.parent.postMessage({ ...req, id, type: "provider-call" }, "*");
     });
   }
 
@@ -64,6 +73,10 @@ export class TariUniverseProvider implements TariProvider {
 
   public async createFreeTestCoins(): Promise<void> {
     return this.sendRequest({ methodName: "createFreeTestCoins", args: [] });
+  }
+
+  public requestParentSize(): Promise<WindowSize> {
+    return this.sendRequest({ methodName: "requestParentSize", args: [] });
   }
 
   public async getAccount(): Promise<Account> {
