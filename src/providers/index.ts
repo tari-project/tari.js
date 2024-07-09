5611e84a8e26752c6f7d20ca4f4b3911d90406b1
Maciej Kożuszek <mackozuszek@gmail.com>
2024-07-09 10:25:55 +0200

    feat(tari-universe): Add support for rejected transaction (#22)
    Description
    ---
    Add field `resultError` to the Tari Universe provider response. If it is
    present in the response than that means the provider call failed and we
    should return `reject` from provider promise.
    
    Motivation and Context
    ---
    After adding transaction confirmation users can decide to cancel
    transaction. This requires from provider to handle case where provider
    denies any transaction to be run.
    
    ![image](https://github.com/tari-project/tari.js/assets/20818447/4f9a7478-fc96-45cf-adcd-8e6101def64e)
    
    
    How Has This Been Tested?
    ---
    Tested manually
    
    What process can a PR reviewer use to test or verify this change?
    ---
    - Setup Tari Universe
    - Run [faucet tapplet](https://github.com/MCozhusheck/faucet-tapplet) in
    dev mode
    - Add this tapplet and click around to see if it's working
    
    Both Tari Universe and faucet tapplet requires tari.js so tester needs
    to add it as local dependency in package.json
    
    <!-- Checklist -->
    <!-- 1. Is the title of your PR in the form that would make nice release
    notes? The title, excluding the conventional commit
    tag, will be included exactly as is in the CHANGELOG, so please think
    about it carefully. -->
    
    
    Breaking Changes
    ---
    
    - [x] None
    - [ ] Requires data directory on base node to be deleted
    - [ ] Requires hard fork
    - [ ] Other - Please specify
    
    <!-- Does this include a breaking change? If so, include this line as a
    footer -->
    <!-- BREAKING CHANGE: Description what the user should do, e.g. delete a
    database, resync the chain -->

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,23 +1,49 @@
-import { SubstateType } from "@tariproject/typescript-bindings";
+import type { SubstateType } from "@tariproject/typescript-bindings";
 import {
-    Account,
-    SubmitTransactionRequest,
-    TransactionResult,
-    SubmitTransactionResponse,
-    VaultBalances,
-    TemplateDefinition, Substate,
-    ListSubstatesResponse,
+  Account,
+  SubmitTransactionRequest,
+  TransactionResult,
+  SubmitTransactionResponse,
+  VaultBalances,
+  TemplateDefinition,
+  Substate,
+  ListSubstatesResponse,
 } from "./types";
+import { ProviderMethods, ProviderMethodNames, ProviderReturnType } from "./tari_universe/types";
+
+export type {
+  Account,
+  SubmitTransactionRequest,
+  TransactionResult,
+  SubmitTransactionResponse,
+  VaultBalances,
+  TemplateDefinition,
+  Substate,
+  ListSubstatesResponse,
+  ProviderMethods,
+  ProviderMethodNames,
+  ProviderReturnType,
+};
 
 export interface TariProvider {
-    providerName: string;
-    isConnected(): boolean;
-    getAccount(): Promise<Account>;
-    getSubstate(substate_address: string): Promise<Substate>,
-    submitTransaction(req: SubmitTransactionRequest): Promise<SubmitTransactionResponse>
-    getTransactionResult(transactionId: string): Promise<TransactionResult>
-    getTemplateDefinition(template_address: string): Promise<TemplateDefinition>
-    getPublicKey(branch: string, index: number): Promise<string>;
-    getConfidentialVaultBalances(viewKeyId: number, vaultId: string, min: number | null, max: number | null): Promise<VaultBalances>;
-    listSubstates(filter_by_template: string | null, filter_by_type: SubstateType | null, limit: number | null, offset: number | null): Promise<ListSubstatesResponse>;
+  providerName: string;
+  isConnected(): boolean;
+  getAccount(): Promise<Account>;
+  getSubstate(substate_address: string): Promise<Substate>;
+  submitTransaction(req: SubmitTransactionRequest): Promise<SubmitTransactionResponse>;
+  getTransactionResult(transactionId: string): Promise<TransactionResult>;
+  getTemplateDefinition(template_address: string): Promise<TemplateDefinition>;
+  getPublicKey(branch: string, index: number): Promise<string>;
+  getConfidentialVaultBalances(
+    viewKeyId: number,
+    vaultId: string,
+    min: number | null,
+    max: number | null,
+  ): Promise<VaultBalances>;
+  listSubstates(
+    filter_by_template: string | null,
+    filter_by_type: SubstateType | null,
+    limit: number | null,
+    offset: number | null,
+  ): Promise<ListSubstatesResponse>;
 }
