423d6b95d8aea7801e42bf009690edc39bde9783
Martins Erts <fp@tari.com>
2025-05-22 10:05:47 +0300

    Update docstring

--- packages/builders/src/helpers/workspace.ts
+++ packages/builders/src/helpers/workspace.ts
@@ -3,12 +3,8 @@ import { WorkspaceArg } from "@tari-project/tarijs-types";
 /**
  *
  * @param key workspace name
- * @returns encoded Uint8Array value
+ * @returns encoded hex value
  * @example
- * key: "0"   -> [ 48 ]
- * key: "0.0" -> [ 48, 46, 48 ]
- * key: "0.1" -> [ 48, 46, 49 ]
- *
  * key: "bucket"   -> "6275636b6574"
  * key: "bucket.0" -> "6275636b65742e30"
  * key: "bucket.1" -> "6275636b65742e31"
