0376de083223f53757d087acc422ea32bbc2cb54
karczuRF <oskar.karcz@rumblefish.pl>
2025-07-15 10:54:28 +0200

    feat: add eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
 
-import eslint from '@eslint/js';
-import tseslint from 'typescript-eslint';
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.recommended,
-);
+export default tseslint.config(eslint.configs.recommended, tseslint.configs.recommended, {
+  rules: {
+    // "no-console": ["warn", { allow: ["info", "warn", "debug", "error"] }],
+    "no-unused-vars": "off", // base rule must be disabled
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": [
+      "warn",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
+});
